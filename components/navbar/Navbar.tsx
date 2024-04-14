1bc3fc6eade993b6368fc94dc9737760fc1baf48
Emily Montalvo <emily.montalvo3006@gmail.com>
2024-04-14 10:32:28 -0500

    fix: change tag a for Link  and automate navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,13 +1,28 @@
+import Link from 'next/link'
 import React from 'react'
+import { HomeIcon } from '@primer/octicons-react'
+import { text } from 'stream/consumers'
+
+const navItems = [
+  { path: '/about', text: 'About'},
+  { path: '/pricing', text: 'Pricing'},
+  { path: '/contact', text: 'Contact'}
+]
 
 const Navbar = () => {
   return (
     <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
+      
+        <Link href={"/"} className='flex items-center'>
+          <HomeIcon className='mr-2'/>
         <span>Home</span>
+        </Link>
+
         <div className='flex flex-1'></div>
-      <a href="/about" className='mr-2'> About</a>
-      <a href="/pricing" className='mr-2'>Pricing</a>
-      <a href="/contact" className='mr-2'>Contact</a>
+
+        {navItems.map(navItem => (
+          <Link key={navItem.path} href={navItem.path} className='mr-2'>{navItem.text}</Link>
+        ))}
     </nav>
   )
 }
